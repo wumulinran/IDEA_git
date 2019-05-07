1ef8175ea9f134e3f85ba5b50cb26a81e0dc5f40
Ran <1156493358@qq.com>
2019-05-07 21:25:42 +0800

    hhh第一次提交

---
+++ src/com/ran/hello/Hello.java
@@ -0,0 +1,25 @@
+package com.ran.hello;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * @Auther: Ran
+ * @Date: 2019/5/5
+ * @Description: com.ran.hello
+ * @version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        System.out.println(date.getTime());
+        System.out.println(date.toLocaleString());
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
+        ArrayList<Object> objects = new ArrayList<>();
+
+
+    }
+    
+}
